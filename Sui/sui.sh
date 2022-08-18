2f69abb9e0367344faa7b695c80f9e3f231debb3
BlackOwl <107190154+brsbrc@users.noreply.github.com>
2022-08-18 04:47:41 +0300

    Create sui.sh

---
+++ Sui/sui.sh
@@ -0,0 +1,58 @@
+#!/bin/bash
+
+echo -e ''
+echo -e '\e[40m\e[92m'
+echo ' ██████╗ ██╗      █████╗  ██████╗██╗  ██╗ ██████╗ ██╗    ██╗██╗'     
+echo ' ██╔══██╗██║     ██╔══██╗██╔════╝██║ ██╔╝██╔═══██╗██║    ██║██║'
+echo ' ██████╔╝██║     ███████║██║     █████╔╝ ██║   ██║██║ █╗ ██║██║'    
+echo ' ██╔══██╗██║     ██╔══██║██║     ██╔═██╗ ██║   ██║██║███╗██║██║'
+echo ' ██████╔╝███████╗██║  ██║╚██████╗██║  ██╗╚██████╔╝╚███╔███╔╝███████╗'
+echo ' ╚═════╝ ╚══════╝╚═╝  ╚═╝ ╚═════╝╚═╝  ╚═╝ ╚═════╝  ╚══╝╚══╝ ╚══════╝'
+echo -e '\e[0m'
+echo ''
+
+sleep 2
+
+echo -e "\e[1m\e[32m1. Güncellemeler Yükleniyor... \e[0m" && sleep 1
+# update
+sudo apt update && sudo apt upgrade -y
+
+echo -e "\e[1m\e[32m2. Yüklemeler Tamamlanıyor... \e[0m" && sleep 1
+# packages
+sudo wget -qO /usr/local/bin/yq https://github.com/mikefarah/yq/releases/download/v4.23.1/yq_linux_amd64 && chmod +x /usr/local/bin/yq
+sudo apt-get install jq -y
+
+# download sui binaries
+version=$(wget -qO- https://api.github.com/repos/SecorD0/Sui/releases/latest | jq -r ".tag_name")
+wget -qO- "https://github.com/SecorD0/Sui/releases/download/${version}/sui-linux-amd64-${version}.tar.gz" | sudo tar -C /usr/local/bin/ -xzf -
+
+# download and update configs
+mkdir -p $HOME/.sui
+wget -qO $HOME/.sui/fullnode.yaml https://github.com/MystenLabs/sui/raw/main/crates/sui-config/data/fullnode-template.yaml
+wget -qO $HOME/.sui/genesis.blob https://github.com/MystenLabs/sui-genesis/raw/main/devnet/genesis.blob
+yq -i ".db-path = \"$HOME/.sui/db\"" $HOME/.sui/fullnode.yaml
+yq -i '.metrics-address = "0.0.0.0:9184"' $HOME/.sui/fullnode.yaml
+yq -i '.json-rpc-address = "0.0.0.0:9000"' $HOME/.sui/fullnode.yaml
+yq -i ".genesis.genesis-file-location = \"$HOME/.sui/genesis.blob\"" $HOME/.sui/fullnode.yaml
+
+# create sui service
+sudo tee /etc/systemd/system/suid.service > /dev/null <<EOF
+[Unit]
+Description=Sui node
+After=network-online.target
+
+[Service]
+User=$USER
+ExecStart=$(which sui-node) --config-path $HOME/.sui/fullnode.yaml
+Restart=on-failure
+RestartSec=3
+LimitNOFILE=65535
+
+[Install]
+WantedBy=multi-user.target
+EOF
+
+# start sui node
+sudo systemctl daemon-reload
+sudo systemctl enable suid
+sudo systemctl restart suid
