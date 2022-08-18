010262ef004a38958b49cbec17691dd1a246a94f
BlackOwl <107190154+brsbrc@users.noreply.github.com>
2022-08-18 04:49:43 +0300

    Create sui-guncelleme.sh

---
+++ Sui/sui-guncelleme.sh
@@ -0,0 +1,12 @@
+echo -e "\e[1m\e[32mSui Full Node Güncelleniyor... \e[0m" && sleep 1
+systemctl stop suid
+wget -qO $HOME/.sui/fullnode.yaml https://github.com/MystenLabs/sui/raw/main/crates/sui-config/data/fullnode-template.yaml
+wget -qO $HOME/.sui/genesis.blob https://github.com/MystenLabs/sui-genesis/raw/main/devnet/genesis.blob
+yq -i ".db-path = \"$HOME/.sui/db\"" $HOME/.sui/fullnode.yaml
+yq -i '.metrics-address = "0.0.0.0:9184"' $HOME/.sui/fullnode.yaml
+yq -i '.json-rpc-address = "0.0.0.0:9000"' $HOME/.sui/fullnode.yaml
+yq -i ".genesis.genesis-file-location = \"$HOME/.sui/genesis.blob\"" $HOME/.sui/fullnode.yaml
+rm -rf $HOME/.sui/db
+version=$(wget -qO- https://api.github.com/repos/SecorD0/Sui/releases/latest | jq -r ".tag_name")
+wget -qO- "https://github.com/SecorD0/Sui/releases/download/${version}/sui-linux-amd64-${version}.tar.gz" | sudo tar -C /usr/local/bin/ -xzf -
+systemctl restart suid
