c4af79cf0f802f6b2b1d5755d23ca9dfdf4b6523
BlackOwl <107190154+brsbrc@users.noreply.github.com>
2022-08-20 19:48:14 +0300

    Create rebus.sh

---
+++ Rebus/rebus.sh
@@ -0,0 +1,128 @@
+#!/bin/bash
+
+echo -e ''
+echo -e '\e[40m\e[92m'
+echo ' ██████╗ ██╗      █████╗  ██████╗██╗  ██╗ ██████╗ ██╗    ██╗██╗'     
+echo ' ██╔══██╗██║     ██╔══██╗██╔════╝██║ ██╔╝██╔═══██╗██║    ██║██║'
+echo ' ██████╔╝██║     ███████║██║     █████╔╝ ██║   ██║██║ █╗ ██║██║'    
+echo ' ██╔══██╗██║     ██╔══██║██║     ██╔═██╗ ██║   ██║██║███╗██║██║'
+echo ' ██████╔╝███████╗██║  ██║╚██████╗██║  ██╗╚██████╔╝╚███╔███╔╝███████╗'
+echo ' ╚═════╝ ╚══════╝╚═╝  ╚═╝ ╚═════╝╚═╝  ╚═╝ ╚═════╝  ╚══╝╚══╝ ╚══════╝'
+echo -e '\e[0m'
+echo ''
+
+sleep 2
+
+# set vars
+if [ ! $NODENAME ]; then
+	read -p "Node isminiz giriniz: " NODENAME
+	echo 'export NODENAME='$NODENAME >> $HOME/.bash_profile
+fi
+REBUS_PORT=21
+if [ ! $WALLET ]; then
+	echo "export WALLET=wallet" >> $HOME/.bash_profile
+fi
+echo "export REBUS_CHAIN_ID=reb_3333-1" >> $HOME/.bash_profile
+echo "export REBUS_PORT=${REBUS_PORT}" >> $HOME/.bash_profile
+source $HOME/.bash_profile
+
+echo '================================================='
+echo -e "Node isminiz: \e[1m\e[32m$NODENAME\e[0m"
+echo -e "Cüzdan isminiz: \e[1m\e[32m$WALLET\e[0m"
+echo -e "Chain id: \e[1m\e[32m$REBUS_CHAIN_ID\e[0m"
+echo -e "Port: \e[1m\e[32m$REBUS_PORT\e[0m"
+echo '================================================='
+sleep 2
+
+echo -e "\e[1m\e[32m1. Sistem güncellemesi yapılıyor... \e[0m" && sleep 1
+# update
+sudo apt update && sudo apt upgrade -y
+
+echo -e "\e[1m\e[32m2. Kütüphaneler kuruluyor... \e[0m" && sleep 1
+# packages
+sudo apt install curl build-essential git wget jq make gcc tmux chrony -y
+
+# install go
+if ! [ -x "$(command -v go)" ]; then
+  ver="1.18.2"
+  cd $HOME
+  wget "https://golang.org/dl/go$ver.linux-amd64.tar.gz"
+  sudo rm -rf /usr/local/go
+  sudo tar -C /usr/local -xzf "go$ver.linux-amd64.tar.gz"
+  rm "go$ver.linux-amd64.tar.gz"
+  echo "export PATH=$PATH:/usr/local/go/bin:$HOME/go/bin" >> ~/.bash_profile
+  source ~/.bash_profile
+fi
+
+echo -e "\e[1m\e[32m3. Binary kurulumu yapılıyor... \e[0m" && sleep 1
+# download binary
+cd $HOME
+git clone https://github.com/rebuschain/rebus.core
+cd rebus.core && git checkout v0.0.3
+make install
+
+# config
+rebusd config chain-id $REBUS_CHAIN_ID
+rebusd config keyring-backend test
+rebusd config node tcp://localhost:${REBUS_PORT}657
+
+# init
+rebusd init $NODENAME --chain-id $REBUS_CHAIN_ID
+
+# download genesis and addrbook
+curl https://raw.githubusercontent.com/rebuschain/rebus.testnet/master/rebus_3333-1/genesis.json > ~/.rebusd/config/genesis.json
+
+# set peers and seeds
+SEEDS="a6d710cd9baac9e95a55525d548850c91f140cd9@3.211.101.169:26656,c296ee829f137cfe020ff293b6fc7d7c3f5eeead@54.157.52.47:26656"
+PEERS=""
+sed -i -e "s/^seeds *=.*/seeds = \"$SEEDS\"/; s/^persistent_peers *=.*/persistent_peers = \"$PEERS\"/" $HOME/.rebusd/config/config.toml
+
+# set custom ports
+sed -i.bak -e "s%^proxy_app = \"tcp://127.0.0.1:26658\"%proxy_app = \"tcp://127.0.0.1:${REBUS_PORT}658\"%; s%^laddr = \"tcp://127.0.0.1:26657\"%laddr = \"tcp://127.0.0.1:${REBUS_PORT}657\"%; s%^pprof_laddr = \"localhost:6060\"%pprof_laddr = \"localhost:${REBUS_PORT}060\"%; s%^laddr = \"tcp://0.0.0.0:26656\"%laddr = \"tcp://0.0.0.0:${REBUS_PORT}656\"%; s%^prometheus_listen_addr = \":26660\"%prometheus_listen_addr = \":${REBUS_PORT}660\"%" $HOME/.rebusd/config/config.toml
+sed -i.bak -e "s%^address = \"tcp://0.0.0.0:1317\"%address = \"tcp://0.0.0.0:${REBUS_PORT}317\"%; s%^address = \":8080\"%address = \":${REBUS_PORT}080\"%; s%^address = \"0.0.0.0:9090\"%address = \"0.0.0.0:${REBUS_PORT}090\"%; s%^address = \"0.0.0.0:9091\"%address = \"0.0.0.0:${REBUS_PORT}091\"%" $HOME/.rebusd/config/app.toml
+
+# config pruning
+pruning="custom"
+pruning_keep_recent="100"
+pruning_keep_every="0"
+pruning_interval="50"
+sed -i -e "s/^pruning *=.*/pruning = \"$pruning\"/" $HOME/.rebusd/config/app.toml
+sed -i -e "s/^pruning-keep-recent *=.*/pruning-keep-recent = \"$pruning_keep_recent\"/" $HOME/.rebusd/config/app.toml
+sed -i -e "s/^pruning-keep-every *=.*/pruning-keep-every = \"$pruning_keep_every\"/" $HOME/.rebusd/config/app.toml
+sed -i -e "s/^pruning-interval *=.*/pruning-interval = \"$pruning_interval\"/" $HOME/.rebusd/config/app.toml
+
+# set minimum gas price and timeout commit
+sed -i -e "s/^minimum-gas-prices *=.*/minimum-gas-prices = \"0ustrd\"/" $HOME/.rebusd/config/app.toml
+
+# enable prometheus
+sed -i -e "s/prometheus = false/prometheus = true/" $HOME/.rebusd/config/config.toml
+
+# reset
+rebusd tendermint unsafe-reset-all --home $HOME/.rebusd
+
+echo -e "\e[1m\e[32m4. Servis Başlatılıyor... \e[0m" && sleep 1
+# create service
+sudo tee /etc/systemd/system/rebusd.service > /dev/null <<EOF
+[Unit]
+Description=rebus
+After=network-online.target
+
+[Service]
+User=$USER
+ExecStart=$(which rebusd) start --home $HOME/.rebusd
+Restart=on-failure
+RestartSec=3
+LimitNOFILE=65535
+
+[Install]
+WantedBy=multi-user.target
+EOF
+
+# start service
+sudo systemctl daemon-reload
+sudo systemctl enable rebusd
+sudo systemctl restart rebusd
+
+echo '=============== Tebrikler, Kurulum Başarıyla Tamamlandı ==================='
+echo -e 'Log kontrol için: \e[1m\e[32mjournalctl -u rebusd -f -o cat\e[0m'
+echo -e "Sync durumu için: \e[1m\e[32mcurl -s localhost:${REBUS_PORT}657/status | jq .result.sync_info\e[0m"
