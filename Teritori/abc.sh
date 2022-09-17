9038aea20951e57bef57cbb3be65ae8ec4eca3af
BlackOwl <107190154+brsbrc@users.noreply.github.com>
2022-09-17 11:37:43 +0300

    aab

--- Teritori/abc.sh
+++ Teritori/abc.sh
@@ -59,7 +59,11 @@ sleep 3
 echo -e "\e[1m\e[32m3. Docker Kuruluyor... \e[0m" && sleep 2
 # Docker Kurulum
 cd /root
-sudo apt install docker.io -y
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu \
+$(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+sudo apt-get update && sudo apt-get install docker-ce docker-ce-cli containerd.io docker-compose-plugin -y </dev/null
+sudo chmod 666 /var/run/docker.sock
 sudo systemctl enable --now docker
 
 sleep 2
