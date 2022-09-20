c7b7683470e548fef56c8abe814c6e7100095d22
BlackOwl <107190154+brsbrc@users.noreply.github.com>
2022-09-20 16:06:35 +0300

    scripts installation

--- NuLink/nulink-en.sh
+++ NuLink/nulink-en.sh
@@ -20,43 +20,64 @@ sudo apt-get -y install libssl-dev && apt-get -y install cmake build-essential g
 
 echo -e "\e[1m\e[32m2. Creating a Worker Account... \e[0m" && sleep 2
 # Account Creation
-wget https://gethstore.blob.core.windows.net/builds/geth-linux-amd64-1.10.24-972007a5.tar.gz
+
+if [ -f geth-linux-amd64-1.10.24-972007a5 ]; then
+	rm -rf geth-linux-amd64-1.10.24-972007a5.tar.gz
+	rm -rf geth-linux-amd64-1.10.24-972007a5
+fi
+
+wget "https://gethstore.blob.core.windows.net/builds/geth-linux-amd64-1.10.24-972007a5.tar.gz"
 tar -xvzf geth-linux-amd64-1.10.24-972007a5.tar.gz
-cd geth-linux-amd64-1.10.24-972007a5/
+
+if [ "$(ls /root/geth-linux-amd64-1.10.24-972007a5/keystore/ | wc -l)" > 1 ]; then
+	rm -rf /root/geth-linux-amd64-1.10.24-972007a5/keystore
+	mkdir /root/geth-linux-amd64-1.10.24-972007a5/keystore
+fi
+
+
+cd $HOME/geth-linux-amd64-1.10.24-972007a5/
 ./geth account new --keystore ./keystore
 
-sleep 13
+sleep 5
 
-# Assignment of the Variable
-if [ ! $KEY ]; then
-	read -p "Type the directory path of the secret key file : " KEY
-	echo 'export KEY='$KEY >> $HOME/.bash_profile
-fi
+sed -i '/UTC/d' $HOME/.bash_profile
+sed -i '/PKEY/d' $HOME/.bash_profile
+sed -i '/KEY/d' $HOME/.bash_profile
+sed -i '/NULINK_KEYSTORE_PASSWORD/d' $HOME/.bash_profile
+sed -i '/NULINK_OPERATOR_ETH_PASSWORD/d' $HOME/.bash_profile
 
-sleep 2
-
-if [ ! $PKEY ]; then
-	read -p "Write your public address: " PKEY
-	echo 'export PKEY='$PKEY >> $HOME/.bash_profile
-fi
-
-sleep 2
-
-if [ ! $UTC ]; then
-	read -p "Write the secret key from the part of your file starting with UTC: " UTC
-	echo 'export UTC='$UTC >> $HOME/.bash_profile
-fi
+UTC="$(ls /root/geth-linux-amd64-1.10.24-972007a5/keystore/)"
+echo "export UTC="$UTC >> $HOME/.bash_profile
+PKEY="0x""$(awk -F \" '{print $4}' /root/geth-linux-amd64-1.10.24-972007a5/keystore/$UTC)"
+echo "export PKEY="$PKEY >> $HOME/.bash_profile
+KEY="/root/geth-linux-amd64-1.10.24-972007a5/keystore/"$UTC
+echo "export KEY="$KEY >> $HOME/.bash_profile
 
 sleep 2
 
-if [ ! $NULINK_PASSWORD ]; then
-	read -p "create a password of 8 characters: " NULINK_PASSWORD
-	echo 'export NULINK_PASSWORD='$NULINK_PASSWORD >> $HOME/.bash_profile
+unset NULINK_KEYSTORE_PASSWORD
+unset NULINK_OPERATOR_ETH_PASSWORD
+
+if [ ! $NULINK_KEYSTORE_PASSWORD ]; then
+	read -p "create a password for NULINK_KEYSTORE_PASSWORD: " NULINK_KEYSTORE_PASSWORD
+	echo 'export NULINK_KEYSTORE_PASSWORD='$NULINK_KEYSTORE_PASSWORD >> $HOME/.bash_profile
+fi
+
+if [ ! $NULINK_OPERATOR_ETH_PASSWORD ]; then
+	read -p "create a password for NULINK_OPERATOR_ETH_PASSWORD: " NULINK_OPERATOR_ETH_PASSWORD
+	echo 'export NULINK_OPERATOR_ETH_PASSWORD='$NULINK_OPERATOR_ETH_PASSWORD >> $HOME/.bash_profile
 fi
 
 source $HOME/.bash_profile
 
+echo -e "Your public address: \e[1m\e[32m$PKEY\e[0m"
+echo -e "Your path to secret key file: \e[1m\e[32m$KEY\e[0m"
+echo -e "Your Nulink Keystore Password: \e[1m\e[32m$NULINK_KEYSTORE_PASSWORD\e[0m"
+echo -e "Your Nulink ETH Operator Password: \e[1m\e[32m$NULINK_OPERATOR_ETH_PASSWORD\e[0m"
+
 sleep 3
+
+docker ps -q --filter "name=ursula" | grep -q . && docker kill ursula && docker rm ursula
 
 echo -e "\e[1m\e[32m3. Installing Docker... \e[0m" && sleep 2
 # Docker İnstall
@@ -73,6 +94,7 @@ sleep 2
 
 # Creating a File
 cd /root
+rm -rf nulink
 mkdir nulink
 
 sleep 2
@@ -89,7 +111,7 @@ docker run -it --rm \
 -p 9151:9151 \
 -v /root/nulink:/code \
 -v /root/nulink:/home/circleci/.local/share/nulink \
--e $NULINK_PASSWORD \
+-e NULINK_KEYSTORE_PASSWORD \
 nulink/nulink nulink ursula init \
 --signer keystore:///code/$UTC \
 --eth-provider https://data-seed-prebsc-2-s2.binance.org:8545  \
@@ -99,7 +121,7 @@ nulink/nulink nulink ursula init \
 --operator-address $PKEY \
 --max-gas-price 100
 
-sleep 12
+sleep 5
 
 # Starting a Node
 docker run --restart on-failure -d \
@@ -107,15 +129,16 @@ docker run --restart on-failure -d \
 -p 9152:9152 \
 -v /root/nulink:/code \
 -v /root/nulink:/home/circleci/.local/share/nulink \
--e $NULINK_PASSWORD \
--e $NULINK_PASSWORD \
+-e NULINK_KEYSTORE_PASSWORD \
+-e NULINK_OPERATOR_ETH_PASSWORD \
 nulink/nulink nulink ursula run --no-block-until-ready
 
-sleep 4
+sleep 3
 
-echo '------------------- The Installation was Completed Successfully. Good luck... -------------------'
-echo '------------------- If you have any questions, you can contact me. Discord id: blackowl#7099... -------------------'
+source $HOME/.bash_profile
 
+echo '----The Installation was Completed Successfully. Good luck... ----'
+echo '---- If you have any questions, you can contact me. Discord id: blackowl#7099... ----'
 
 
 
