f21245c996d4d9711424f4019a9364a853f743c4
BlackOwl <107190154+brsbrc@users.noreply.github.com>
2022-10-22 10:52:35 +0300

    v0.9.5

--- Wormholes/wormholes.sh
+++ Wormholes/wormholes.sh
@@ -24,8 +24,10 @@ docker rmi wormholestech/wormholes:v1 > /dev/null 2>&1
 if [ -d /wm/.wormholes/keystore ]; then
    read -p "Whether to clear the Wormholes blockchain data history, if yes, press the “y” button, and if not, click “enter.”：" xyz
    if [ "$xyz" = 'y' ]; then
+         cp /wm/.wormholes/wormholes/nodekey /wm/nodekey
          rm -rf /wm/.wormholes
-              read -p "Enter your private key：" ky
+         mkdir -p /wm/.wormholes/wormholes
+         mv /wm/nodekey /wm/.wormholes/wormholes/
    else
          echo "Do not clear"
    fi
@@ -33,13 +35,12 @@ else
    read -p "Please import your private key：" ky
 fi
 
-mkdir -p /wm/.wormholes/wormholes
 if [ -n "$ky" ]; then
-   echo ${#ky}
-     echo ${ky:0:2}
      if [ ${#ky} -eq 64 ];then
+             mkdir -p /wm/.wormholes/wormholes
              echo $ky > /wm/.wormholes/wormholes/nodekey
      elif [ ${#ky} -eq 66 ] && ([ ${ky:0:2} == "0x" ] || [ ${ky:0:2} == "0X" ]);then
+             mkdir -p /wm/.wormholes/wormholes
              echo ${ky:2:64} > /wm/.wormholes/wormholes/nodekey
      else
              echo "the nodekey format is not correct"
@@ -47,7 +48,7 @@ if [ -n "$ky" ]; then
      fi
 fi
 
-docker run -id -e KEY=$ky  -p 30303:30303 -p 8545:8545 -v /wm/.wormholes:/wm/.wormholes --name wormholes wormholestech/wormholes:v1
+docker run -id -p 30303:30303 -p 8545:8545 -v /wm/.wormholes:/wm/.wormholes --name wormholes wormholestech/wormholes:v1
 
 echo "Your private key is:"
 sleep 6
