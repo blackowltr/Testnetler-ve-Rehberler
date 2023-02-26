0953483f1ad43e992efbdff503b5513b43e1c7d6
BlackOwl <107190154+brsbrc@users.noreply.github.com>
2023-02-27 00:48:36 +0300

    Create lava.sh

---
+++ Lava/lava.sh
@@ -0,0 +1,77 @@
+#!/bin/bash
+
+read -r -p "Node Adınızı Yazın: " NODE_MONIKER
+
+CHAIN_ID="lava-testnet-1"
+CHAIN_DENOM="ulava"
+BINARY_NAME="lavad"
+BINARY_VERSION_TAG="v0.6.0"
+CHEAT_SHEET="https://nodejumper.io/lava-testnet/cheat-sheet"
+
+printLine
+echo -e "Node moniker:       ${CYAN}$NODE_MONIKER${NC}"
+echo -e "Chain id:           ${CYAN}$CHAIN_ID${NC}"
+echo -e "Chain demon:        ${CYAN}$CHAIN_DENOM${NC}"
+echo -e "Binary version tag: ${CYAN}$BINARY_VERSION_TAG${NC}"
+printLine
+sleep 1
+
+source <(curl -s https://raw.githubusercontent.com/nodejumper-org/cosmos-scripts/master/utils/dependencies_install.sh)
+
+printCyan "4. Binary kuruluyor..." && sleep 1
+
+cd || return
+rm -rf lava
+git clone https://github.com/lavanet/lava
+cd lava || return
+git checkout v0.6.0-RC3
+make install
+lavad version
+
+lavad config keyring-backend test
+lavad config chain-id $CHAIN_ID
+lavad init "$NODE_MONIKER" --chain-id $CHAIN_ID
+
+curl -s https://raw.githubusercontent.com/K433QLtr6RA9ExEq/GHFkqmTzpdNLDd6T/main/testnet-1/genesis_json/genesis.json > $HOME/.lava/config/genesis.json
+curl -s https://snapshots1-testnet.nodejumper.io/lava-testnet/addrbook.json > $HOME/.lava/config/addrbook.json
+
+SEEDS="3a445bfdbe2d0c8ee82461633aa3af31bc2b4dc0@prod-pnet-seed-node.lavanet.xyz:26656,e593c7a9ca61f5616119d6beb5bd8ef5dd28d62d@prod-pnet-seed-node2.lavanet.xyz:26656"
+PEERS=""
+sed -i 's|^seeds *=.*|seeds = "'$SEEDS'"|; s|^persistent_peers *=.*|persistent_peers = "'$PEERS'"|' $HOME/.lava/config/config.toml
+
+sed -i 's|^pruning *=.*|pruning = "custom"|g' $HOME/.lava/config/app.toml
+sed -i 's|^pruning-keep-recent  *=.*|pruning-keep-recent = "100"|g' $HOME/.lava/config/app.toml
+sed -i 's|^pruning-interval *=.*|pruning-interval = "10"|g' $HOME/.lava/config/app.toml
+sed -i 's|^snapshot-interval *=.*|snapshot-interval = 2000|g' $HOME/.lava/config/app.toml
+
+sed -i 's|^minimum-gas-prices *=.*|minimum-gas-prices = "0.025ulava"|g' $HOME/.lava/config/app.toml
+sed -i 's|^prometheus *=.*|prometheus = true|' $HOME/.lava/config/config.toml
+
+printCyan "5. Servis dosyası hazırlanıyor..." && sleep 1
+
+sudo tee /etc/systemd/system/lavad.service > /dev/null << EOF
+[Unit]
+Description=Lava Network Node
+After=network-online.target
+[Service]
+User=$USER
+ExecStart=$(which lavad) start
+Restart=on-failure
+RestartSec=10
+LimitNOFILE=10000
+[Install]
+WantedBy=multi-user.target
+EOF
+
+lavad tendermint unsafe-reset-all --home $HOME/.lava --keep-addr-book
+
+SNAP_NAME=$(curl -s https://snapshots1-testnet.nodejumper.io/lava-testnet/info.json | jq -r .fileName)
+curl "https://snapshots1-testnet.nodejumper.io/lava-testnet/${SNAP_NAME}" | lz4 -dc - | tar -xf - -C "$HOME/.lava"
+
+sudo systemctl daemon-reload
+sudo systemctl enable lavad
+sudo systemctl start lavad
+
+printLine
+echo -e "Log Kontrol:            ${CYAN}sudo journalctl -u $BINARY_NAME -f --no-hostname -o cat ${NC}"
+echo -e "Sync Durumu: ${CYAN}$BINARY_NAME status 2>&1 | jq .SyncInfo.catching_up${NC}"
