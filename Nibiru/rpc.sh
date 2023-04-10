d2878d64619c40fff59f64f3332e37c5726a3070
BlackOwl <107190154+brsbrc@users.noreply.github.com>
2023-04-11 01:28:21 +0300

    rpc değiştirme

---
+++ Nibiru/rpc.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+echo -e ''
+echo -e '\e[40m\e[92m'
+echo ' ██████╗ ██╗      █████╗  ██████╗██╗  ██╗ ██████╗ ██╗    ██╗██╗'     
+echo ' ██╔══██╗██║     ██╔══██╗██╔════╝██║ ██╔╝██╔═══██╗██║    ██║██║'
+echo ' ██████╔╝██║     ███████║██║     █████╔╝ ██║   ██║██║ █╗ ██║██║'    
+echo ' ██╔══██╗██║     ██╔══██║██║     ██╔═██╗ ██║   ██║██║███╗██║██║'
+echo ' ██████╔╝███████╗██║  ██║╚██████╗██║  ██╗╚██████╔╝╚███╔███╔╝███████╗'
+echo ' ╚═════╝ ╚══════╝╚═╝  ╚═╝ ╚═════╝╚═╝  ╚═╝ ╚═════╝  ╚══╝╚══╝ ╚══════╝'
+echo -e '\e[0m'
+echo ''
+
+sleep 1
+
+RPC="https://nibiru-testnet.nodejumper.io:443"
+sed -i -e "s|^node *=.*|node = \"$RPC\"|" $HOME/.nibid/config/client.toml
+
+systemctl restart nibid
+
+echo "İşlem Tamamlandı..."
+echo "Sorularınız olursa Nibiru discord TR kanalından sorabilirsiniz."
