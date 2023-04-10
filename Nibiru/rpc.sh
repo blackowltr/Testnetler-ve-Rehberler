72fb3e092d28b089f7082d7244917df9bd530bf4
BlackOwl <107190154+brsbrc@users.noreply.github.com>
2023-04-11 01:39:18 +0300

    RPC

--- Nibiru/rpc.sh
+++ Nibiru/rpc.sh
@@ -11,12 +11,14 @@ echo ' ╚═════╝ ╚══════╝╚═╝  ╚═╝ ╚═
 echo -e '\e[0m'
 echo ''
 
-sleep 1
+sleep 2
 
 RPC="https://nibiru-testnet.nodejumper.io:443"
 sed -i -e "s|^node *=.*|node = \"$RPC\"|" $HOME/.nibid/config/client.toml
 
 systemctl restart nibid
+
+sleep 1
 
 echo "İşlem Tamamlandı..."
 echo "Testnete dair sorunuz olursa Nibiru discord TR kanalından sorabilirsiniz."
