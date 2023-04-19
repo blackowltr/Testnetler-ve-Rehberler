576cc4af053384632dc407d0f138e994b0948e63
BlackOwl <107190154+brsbrc@users.noreply.github.com>
2023-04-19 17:18:39 +0300

    Update nibid.sh

--- Nibiru/nibid.sh
+++ Nibiru/nibid.sh
@@ -126,9 +126,11 @@ sudo systemctl stop nibid
 cp $HOME/.nibid/data/priv_validator_state.json $HOME/.nibid/priv_validator_state.json.backup 
 
 nibid tendermint unsafe-reset-all --home $HOME/.nibid --keep-addr-book 
-curl https://snapshots2-testnet.nodejumper.io/nibiru-testnet/nibiru-itn-1_2023-04-18.tar.lz4 | lz4 -dc - | tar -xf - -C $HOME/.nibid
+curl https://snapshots2-testnet.nodejumper.io/nibiru-testnet/nibiru-itn-1_2023-04-19.tar.lz4 | lz4 -dc - | tar -xf - -C $HOME/.nibid
 
 mv $HOME/.nibid/priv_validator_state.json.backup $HOME/.nibid/data/priv_validator_state.json 
+
+sudo systemctl restart nibid
 
 # rpc 
 RPC="https://nibiru-testnet.nodejumper.io:443"
