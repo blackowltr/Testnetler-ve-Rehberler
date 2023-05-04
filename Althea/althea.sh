13a453f150e205b265ec3533974a41d74961fb1f
BlackOwl <107190154+brsbrc@users.noreply.github.com>
2023-05-04 14:04:26 +0300

    Update althea.sh

--- Althea/althea.sh
+++ Althea/althea.sh
@@ -26,7 +26,7 @@ echo -e "Node isminiz: \e[1m\e[32m$NODENAME\e[0m"
 echo -e "Chain id: \e[1m\e[32m$ALTHEA_CHAIN_ID\e[0m"
 echo '================================================='
 sleep 2
-
+# binary yükleme
 cd || return
 rm -rf althea-chain
 git clone https://github.com/althea-net/althea-chain
@@ -38,14 +38,14 @@ althea version # v0.3.2
 althea config keyring-backend test
 althea config chain-id althea_7357-1
 althea init "$NODENAME" --chain-id althea_7357-1
-
+# addrbook ve genesis.json 
 curl -s https://raw.githubusercontent.com/althea-net/althea-chain-docs/main/testnet-3-genesis.json > $HOME/.althea/config/genesis.json
 curl -s https://snapshots2-testnet.nodejumper.io/althea-testnet/addrbook.json > $HOME/.althea/config/addrbook.json
-
+# peer ve seed ayarlama
 SEEDS=""
 PEERS=""
 sed -i 's|^seeds *=.*|seeds = "'$SEEDS'"|; s|^persistent_peers *=.*|persistent_peers = "'$PEERS'"|' $HOME/.althea/config/config.toml
-
+# pruning ve indexer
 sed -i 's|^pruning *=.*|pruning = "custom"|g' $HOME/.althea/config/app.toml
 sed -i 's|^pruning-keep-recent  *=.*|pruning-keep-recent = "100"|g' $HOME/.althea/config/app.toml
 sed -i 's|^pruning-interval *=.*|pruning-interval = "10"|g' $HOME/.althea/config/app.toml
@@ -54,6 +54,8 @@ sed -i 's|^snapshot-interval *=.*|snapshot-interval = 0|g' $HOME/.althea/config/
 sed -i 's|^minimum-gas-prices *=.*|minimum-gas-prices = "0.0001ualthea"|g' $HOME/.althea/config/app.toml
 sed -i 's|^prometheus *=.*|prometheus = true|' $HOME/.althea/config/config.toml
 
+sed -i -e 's|^indexer *=.*|indexer = "null"|' $HOME/.althea/config/config.toml
+# servis baslatma
 sudo tee /etc/systemd/system/althead.service > /dev/null << EOF
 [Unit]
 Description=Althea Node
@@ -69,10 +71,10 @@ WantedBy=multi-user.target
 EOF
 
 althea tendermint unsafe-reset-all --home $HOME/.althea --keep-addr-book
-
+# snapshot
 SNAP_NAME=$(curl -s https://snapshots2-testnet.nodejumper.io/althea-testnet/info.json | jq -r .fileName)
 curl "https://snapshots2-testnet.nodejumper.io/althea-testnet/${SNAP_NAME}" | lz4 -dc - | tar -xf - -C $HOME/.althea
-
+# baslatma
 sudo systemctl daemon-reload
 sudo systemctl enable althead
 sudo systemctl start althead
