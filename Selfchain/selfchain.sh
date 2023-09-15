747e0cc76f08b3430afcebbc07a91d87ab15eaac
BlackOwl <107190154+brsbrc@users.noreply.github.com>
2023-09-16 02:26:52 +0300

    Update selfchain.sh

--- Selfchain/selfchain.sh
+++ Selfchain/selfchain.sh
@@ -1,5 +1,16 @@
 #!/bin/bash
 
+# Renk kodları
+GREEN='\e[1;32m'
+YELLOW='\e[1;33m'
+CYAN='\e[1;36m'
+NC='\e[0m' # Renk sıfırlama
+
+# Başlık fonksiyonu
+print_title() {
+    echo -e "${CYAN}## $1${NC}\n"
+}
+
 echo -e '\e[40m\e[92m'
 echo ' ██████╗ ██╗      █████╗  ██████╗██╗  ██╗ ██████╗ ██╗    ██╗██╗'
 echo ' ██╔══██╗██║     ██╔══██╗██╔════╝██║ ██╔╝██╔═══██╗██║    ██║██║'
@@ -11,31 +22,25 @@ echo -e '\e[0m'
 
 # Değişkenler
 if [ ! $NODENAME ]; then
-	read -p $'\e[1m\e[93mLütfen node ismini giriniz: \e[0m' NODENAME
-	echo 'export NODENAME='$NODENAME >> $HOME/.bash_profile
+    read -p "Node ismini giriniz: " NODENAME
+    echo "export NODENAME=$NODENAME" >> $HOME/.bash_profile
 fi
 
 if [ ! $WALLET ]; then
-	echo "export WALLET=wallet" >> $HOME/.bash_profile
+    echo "export WALLET=wallet" >> $HOME/.bash_profile
 fi
 
 SELF_CHAIN_ID=self-dev-1
 source $HOME/.bash_profile
 
-echo -e '\e[1m\e[32m================================================='
-echo -e "Node İsmi: $NODENAME"
-echo -e "Cüzdan İsmi: $WALLET"
-echo -e "Chain ID: $SELF_CHAIN_ID"
-echo -e '=================================================\e[0m'
-sleep 2
-
-# Adım 1: Sistemi Güncelleme
-echo -e '\e[1m\e[34mAdım 1: Sistemi Güncelleme...\e[0m'
+# Başlıklar
+print_title "Step 1: Update the System"
+echo -e "${YELLOW}Updating the system...${NC}"
 sudo apt update
 sudo apt-get install git curl build-essential make jq gcc snapd chrony lz4 tmux unzip bc -y
 
-# Adım 2: Go Kurma
-echo -e '\e[1m\e[34mAdım 2: Go Kurma...\e[0m'
+print_title "Step 2: Install Go"
+echo -e "${YELLOW}Installing Go...${NC}"
 rm -rf $HOME/go
 sudo rm -rf /usr/local/go
 cd $HOME
@@ -49,47 +54,52 @@ EOF
 source $HOME/.profile
 go version
 
-# Adım 3: Node Kurma
-echo -e '\e[1m\e[34mAdım 3: Node Kurma...\e[0m'
+print_title "Step 3: Install Node"
+echo -e "${YELLOW}Installing Node...${NC}"
 cd $HOME
 mkdir -p /root/go/bin/
 wget https://ss-t.self.nodestake.top/selfchaind
 chmod +x selfchaind
 mv selfchaind /root/go/bin/
 
-# Adım 4: Node Başlatma
-echo -e '\e[1m\e[34mAdım 4: Node Başlatma...\e[0m'
+print_title "Step 4: Initialize Node"
+echo -e "${YELLOW}Initializing Node...${NC}"
 selfchaind init $NODENAME --chain-id=self-dev-1
 
-# Genesis'i İndirme
-echo -e '\e[1m\e[34mGenesis'i İndirme...\e[0m'
-wget -O .selfchain/config/genesis.json  https://raw.githubusercontent.com/hotcrosscom/selfchain-genesis/main/networks/devnet/genesis.json
+print_title "Download Genesis"
+echo -e "${YELLOW}Downloading Genesis...${NC}"
+curl -Ls https://raw.githubusercontent.com/brsbrc/Testnetler-ve-Rehberler/main/Selfchain/genesis.json > $HOME/.selfchain/config/genesis.json
 
-# Addrbook'u İndirme
-echo -e '\e[1m\e[34mAddrbook'u İndirme...\e[0m'
-curl -Ls https://github.com/Adamtruong6868/Selfchain.xyz/blob/main/addrbook.json > $HOME/.selfchain/config/addrbook.json
+print_title "Download addrbook"
+echo -e "${YELLOW}Downloading addrbook...${NC}"
+curl -Ls https://raw.githubusercontent.com/brsbrc/Testnetler-ve-Rehberler/main/Selfchain/addrbook.json > $HOME/.selfchain/config/addrbook.json
 
-# Servis Oluşturma
-echo -e '\e[1m\e[34mServis Oluşturma...\e[0m'
+print_title "Create Service"
+echo -e "${YELLOW}Creating Service...${NC}"
 sudo tee /etc/systemd/system/selfchaind.service > /dev/null <<EOF
 [Unit]
 Description=selfchaind Daemon
 After=network-online.target
-
 [Service]
 User=$USER
 ExecStart=$(which selfchaind) start
 Restart=always
 RestartSec=3
 LimitNOFILE=65535
-
 [Install]
 WantedBy=multi-user.target
 EOF
 sudo systemctl daemon-reload
 sudo systemctl enable selfchaind
 sudo systemctl start selfchaind
-```
 
-echo -e '\e[1m\e[32mSelfchain Node Kurulumu Tamamlandı.'
-echo -e 'Beni takip etmeyi unutmayın: \e[4m\e[96mhttps://twitter.com/brsbtc\e[0m'
+print_title "Snapshot"
+echo -e "${YELLOW}Performing Snapshot...${NC}"
+SNAP_NAME=$(curl -s https://ss-t.self.nodestake.top/ | egrep -o ">20.*\.tar.lz4" | tr -d ">")
+curl -o - -L https://ss-t.self.nodestake.top/${SNAP_NAME} | lz4 -c -d - | tar -x -C $HOME/.selfchain
+sudo systemctl restart selfchaind
+journalctl -u selfchaind -f
+
+# Tamamlandı mesajı
+echo -e "${GREEN}Selfchain Node Kurulumu Tamamlandı.${NC}"
+echo -e "Beni takip etmeyi unutmayın: \e[4m\e[96mhttps://twitter.com/brsbtc\e[0m"
