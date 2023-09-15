5e9f450043787aed3e7d5b54627ca622a7debd81
BlackOwl <107190154+brsbrc@users.noreply.github.com>
2023-09-16 01:51:11 +0300

    Create selfchain.sh

---
+++ Selfchain/selfchain.sh
@@ -0,0 +1,87 @@
+#!/bin/bash
+
+echo -e ''
+echo -e '\e[40m\e[92m'
+echo ' ██████╗ ██╗      █████╗  ██████╗██╗  ██╗ ██████╗ ██╗    ██╗██╗'     
+echo ' ██╔══██╗██║     ██╔══██╗██╔════╝██║ ██╔╝██╔═══██╗██║    ██║██║'
+echo ' ██████╔╝██║     ███████║██║     █████╔╝ ██║   ██║██║ █╗ ██║██║'    
+echo ' ██╔══██╗██║     ██╔══██║██║     ██╔═██╗ ██║   ██║██║███╗██║██║'
+echo ' ██████╔╝███████╗██║  ██║╚██████╗██║  ██╗╚██████╔╝╚███╔███╔╝███████╗'
+echo ' ╚═════╝ ╚══════╝╚═╝  ╚═╝ ╚═════╝╚═╝  ╚═╝ ╚═════╝  ╚══╝╚══╝ ╚══════╝'
+echo -e '\e[0m'
+echo ''
+
+# degiskenler
+if [ ! $NODENAME ]; then
+	read -p "Node isminiz giriniz: " NODENAME
+	echo 'export NODENAME='$NODENAME >> $HOME/.bash_profile
+fi
+if [ ! $WALLET ]; then
+	echo "export WALLET=wallet" >> $HOME/.bash_profile
+fi
+SELF_CHAIN_ID=self-dev-1
+source $HOME/.bash_profile
+
+echo '================================================='
+echo -e "Node isminiz: \e[1m\e[32m$NODENAME\e[0m"
+echo -e "Cüzdan isminiz: \e[1m\e[32m$WALLET\e[0m"
+echo -e "Chain id: \e[1m\e[32m$SELF_CHAIN_ID\e[0m"
+echo '================================================='
+sleep 2
+
+# Step 1: Update the System
+sudo apt update
+sudo apt-get install git curl build-essential make jq gcc snapd chrony lz4 tmux unzip bc -y
+
+# Step 2: Install Go
+rm -rf $HOME/go
+sudo rm -rf /usr/local/go
+cd $HOME
+curl https://dl.google.com/go/go1.20.5.linux-amd64.tar.gz | sudo tar -C/usr/local -zxvf -
+cat <<'EOF' >>$HOME/.profile
+export GOROOT=/usr/local/go
+export GOPATH=$HOME/go
+export GO111MODULE=on
+export PATH=$PATH:/usr/local/go/bin:$HOME/go/bin
+EOF
+source $HOME/.profile
+go version
+
+# Step 3: Install Node
+cd $HOME
+mkdir -p /root/go/bin/
+wget https://ss-t.self.nodestake.top/selfchaind
+chmod +x selfchaind
+mv selfchaind /root/go/bin/
+
+# Step 4: Initialize Node
+selfchaind init <NodeName> --chain-id=self-dev-1
+
+# Download Genesis
+curl -Ls https://github.com/Adamtruong6868/Selfchain.xyz/blob/main/genesis.json > $HOME/.selfchain/config/genesis.json
+
+# Download addrbook
+curl -Ls https://github.com/Adamtruong6868/Selfchain.xyz/blob/main/addrbook.json > $HOME/.selfchain/config/addrbook.json
+
+# Create Service
+sudo tee /etc/systemd/system/selfchaind.service > /dev/null <<EOF
+[Unit]
+Description=selfchaind Daemon
+After=network-online.target
+
+[Service]
+User=$USER
+ExecStart=$(which selfchaind) start
+Restart=always
+RestartSec=3
+LimitNOFILE=65535
+
+[Install]
+WantedBy=multi-user.target
+EOF
+sudo systemctl daemon-reload
+sudo systemctl enable selfchaind
+sudo systemctl start selfchaind
+
+echo "Selfchain Node Setup Completed."
+echo "Don't forget to follow my account: https://twitter.com/brsbtc"
