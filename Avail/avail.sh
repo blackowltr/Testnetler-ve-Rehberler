f9190329425951fcb87f77cd6998e6ec355ee338
BlackOwl <107190154+brsbrc@users.noreply.github.com>
2023-11-03 09:46:34 +0300

    Update avail.sh

--- Avail/avail.sh
+++ Avail/avail.sh
@@ -15,26 +15,27 @@ echo ''
 read -p "Lütfen bir moniker girin: " moniker
 
 # Sistem güncelleme 
-sudo apt update && sudo apt upgrade -y
+sudo apt update
+sudo apt upgrade -y
 sleep 2
 
 # Gerekli kütüphaneler
-sudo apt install make clang pkg-config libssl-dev build-essential git screen protobuf-compiler -y
+sudo apt install -y make clang pkg-config libssl-dev build-essential git screen protobuf-compiler
 sleep 2
 
-# rust
+# Rust
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
 sleep 2
 
-# rust ortam değişkeni ekleme
+# Rust ortam değişkeni ekleme
 source $HOME/.cargo/env
 sleep 2
 
-# rust güncelleme
-rustup update nightly
+# Rust güncelleme
+rustup update
 sleep 2
 
-# rust
+# Rust target ekleme
 rustup target add wasm32-unknown-unknown --toolchain nightly
 sleep 2
 
@@ -42,27 +43,27 @@ sleep 2
 git clone https://github.com/availproject/avail.git
 sleep 2
 
-# avail klasörüne girme
+# Avail klasörüne girme
 cd avail
 sleep 2
 
-# derleme
-cargo build --release -p data-avail
+# Derleme
+cargo build --release
 sleep 2
 
-# yeni dizin
+# Yeni dizin
 mkdir -p output
 sleep 2
 
-# git checkout
+# Git checkout
 git checkout v1.7.2
 sleep 2
 
-# cargo aracı ile rust uygulamasını çalıştırmak
+# Cargo aracı ile Rust uygulamasını çalıştırma
 cargo run --locked --release -- --chain kate -d ./output
 sleep 2
 
-# servis oluşturma
+# Servis oluşturma
 sudo tee /etc/systemd/system/availd.service <<EOF
 [Unit]
 Description=Avail Validator
@@ -78,8 +79,6 @@ WantedBy=multi-user.target
 EOF
 sleep 2
 
-# Onbeşinci komut
+# servisi başlatma
 sudo systemctl enable availd.service
 sudo systemctl start availd.service
-
-
