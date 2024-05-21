40d0492cd578dafc0a7b1ff7eb59b88af71debf5
BlackOwl <107190154+blackowltr@users.noreply.github.com>
2024-05-21 13:45:25 +0300

    Update rpc-endpoint.sh

--- Initia/rpc-endpoint.sh
+++ Initia/rpc-endpoint.sh
@@ -16,39 +16,38 @@ IP=$(wget -qO- eth0.me)
 RPC_PORT=$(grep -A 3 "\[rpc\]" $HOME/.initia/config/config.toml | grep -oP ":\K[0-9]+")
 RPC_URL="http://$IP:$RPC_PORT"
 
-# RPC bağlantısını kontrol et
-check_rpc_connection
-
-# Eğer bağlantı başarısızsa, gerekli işlemleri yap
-if [ "$SUCCESS" = "0" ]; then
-    # Config dosyasını düzenle ve node'u yeniden başlat
-    sed -i '/\[rpc\]/,/\[/{s/^laddr = "tcp:\/\/127\.0\.0\.1:/laddr = "tcp:\/\/0.0.0\.0:/}' $HOME/.initia/config/config.toml
-    sudo systemctl restart initiad
-    sleep 5
-
-    # Yeniden RPC bağlantısını kontrol et
+# Bağlantı başarılı olana kadar döngü
+SUCCESS=0
+while [ $SUCCESS -eq 0 ]; do
+    # RPC bağlantısını kontrol et
     check_rpc_connection
-
-    # Eğer hala bağlantı başarısızsa, firewall'u kontrol et ve bağlantı noktasını aç
+    
+    # Eğer bağlantı başarısızsa, gerekli işlemleri yap
     if [ "$SUCCESS" = "0" ]; then
-        PORT=$(grep -A 3 "\[rpc\]" $HOME/.initia/config/config.toml | egrep -o ":[0-9]+") && \
-        PORT=${PORT#:} && \
-        sudo ufw allow $PORT/tcp
+        # Config dosyasını düzenle ve node'u yeniden başlat
+        sed -i '/\[rpc\]/,/\[/{s/^laddr = "tcp:\/\/127\.0\.0\.1:/laddr = "tcp:\/\/0.0.0\.0:/}' $HOME/.initia/config/config.toml
         sudo systemctl restart initiad
         sleep 5
 
-        # Son kez RPC bağlantısını kontrol et
+        # Yeniden RPC bağlantısını kontrol et
         check_rpc_connection
+
+        # Eğer hala bağlantı başarısızsa, firewall'u kontrol et ve bağlantı noktasını aç
+        if [ "$SUCCESS" = "0" ]; then
+            PORT=$(grep -A 3 "\[rpc\]" $HOME/.initia/config/config.toml | egrep -o ":[0-9]+") && \
+            PORT=${PORT#:} && \
+            sudo ufw allow $PORT/tcp
+            sudo systemctl restart initiad
+            sleep 5
+
+            # Son kez RPC bağlantısını kontrol et
+            check_rpc_connection
+        fi
     fi
-fi
+done
 
-# Sonuçları yazdır
-if [ "$SUCCESS" = "1" ]; then
-    echo -e "\n\033[1;32mRPC BAĞLANTISI BAŞARILI\033[0m"
-    echo -e "Public RPC URL'si: $RPC_URL"
-else
-    echo -e "\n\033[1;31mRPC bağlantısı başarısız oldu.\033[0m"
-fi
-
-# Beni takip et
+# RPC bağlantısı başarılı oldu
+echo -e "\n\033[1;32mRPC BAĞLANTISI BAŞARILI\033[0m"
+echo -e "Public RPC URL'si: $RPC_URL"
 echo -e "\nBeni X'te takip edin: https://x.com/brsbtc"
+
