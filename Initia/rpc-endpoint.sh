7a03a18320890e35b0575a79c9657da725feeb54
BlackOwl <107190154+blackowltr@users.noreply.github.com>
2024-05-21 13:48:06 +0300

    Update rpc-endpoint.sh

--- Initia/rpc-endpoint.sh
+++ Initia/rpc-endpoint.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-# Fonksiyon: RPC bağlantısını kontrol et
+# RPC bağlantısını kontrol et
 check_rpc_connection() {
-    if curl $RPC_URL/status; then
+    if curl $RPC_URL/status &>/dev/null; then
         SUCCESS=1
     else
         SUCCESS=0
@@ -16,7 +16,7 @@ IP=$(wget -qO- eth0.me)
 RPC_PORT=$(grep -A 3 "\[rpc\]" $HOME/.initia/config/config.toml | grep -oP ":\K[0-9]+")
 RPC_URL="http://$IP:$RPC_PORT"
 
-# Bağlantı başarılı olana kadar döngü
+# Bağlantı başarılı olana kadar döngüye gir
 SUCCESS=0
 while [ $SUCCESS -eq 0 ]; do
     # RPC bağlantısını kontrol et
@@ -26,7 +26,7 @@ while [ $SUCCESS -eq 0 ]; do
     if [ "$SUCCESS" = "0" ]; then
         # Config dosyasını düzenle ve node'u yeniden başlat
         sed -i '/\[rpc\]/,/\[/{s/^laddr = "tcp:\/\/127\.0\.0\.1:/laddr = "tcp:\/\/0.0.0\.0:/}' $HOME/.initia/config/config.toml
-        sudo systemctl restart initiad
+        sudo systemctl restart initiad &>/dev/null
         sleep 5
 
         # Yeniden RPC bağlantısını kontrol et
@@ -36,8 +36,8 @@ while [ $SUCCESS -eq 0 ]; do
         if [ "$SUCCESS" = "0" ]; then
             PORT=$(grep -A 3 "\[rpc\]" $HOME/.initia/config/config.toml | egrep -o ":[0-9]+") && \
             PORT=${PORT#:} && \
-            sudo ufw allow $PORT/tcp
-            sudo systemctl restart initiad
+            sudo ufw allow $PORT/tcp &>/dev/null
+            sudo systemctl restart initiad &>/dev/null
             sleep 5
 
             # Son kez RPC bağlantısını kontrol et
@@ -50,4 +50,3 @@ done
 echo -e "\n\033[1;32mRPC BAĞLANTISI BAŞARILI\033[0m"
 echo -e "Public RPC URL'si: $RPC_URL"
 echo -e "\nBeni X'te takip edin: https://x.com/brsbtc"
-
