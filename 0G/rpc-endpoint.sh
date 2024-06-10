fd08c0b9ad63a6345dd5513172276f7f5bec5d92
BlackOwl <107190154+blackowltr@users.noreply.github.com>
2024-06-10 17:50:48 +0300

    Create rpc-endpoint.sh

---
+++ 0G/rpc-endpoint.sh
@@ -0,0 +1,52 @@
+#!/bin/bash
+
+# Fonksiyon: RPC bağlantısını kontrol et
+check_rpc_connection() {
+    if curl $RPC_URL/status &>/dev/null; then
+        SUCCESS=1
+    else
+        SUCCESS=0
+    fi
+}
+
+# IP adresini al
+IP=$(wget -qO- eth0.me)
+
+# RPC bağlantı noktasını al
+RPC_PORT=$(grep -A 3 "\[rpc\]" $HOME/.0gchain/config/config.toml | grep -oP ":\K[0-9]+")
+RPC_URL="http://$IP:$RPC_PORT"
+
+# Bağlantı başarılı olana kadar döngü
+SUCCESS=0
+while [ $SUCCESS -eq 0 ]; do
+    # RPC bağlantısını kontrol et
+    check_rpc_connection
+    
+    # Eğer bağlantı başarısızsa, gerekli işlemleri yap
+    if [ "$SUCCESS" = "0" ]; then
+        # Config dosyasını düzenle ve node'u yeniden başlat
+        sed -i '/\[rpc\]/,/\[/{s/^laddr = "tcp:\/\/127\.0\.0\.1:/laddr = "tcp:\/\/0.0.0\.0:/}' $HOME/.0gchain/config/config.toml
+        sudo systemctl restart 0gchaind &>/dev/null
+        sleep 5
+
+        # Yeniden RPC bağlantısını kontrol et
+        check_rpc_connection
+
+        # Eğer hala bağlantı başarısızsa, firewall'u kontrol et ve bağlantı noktasını aç
+        if [ "$SUCCESS" = "0" ]; then
+            PORT=$(grep -A 3 "\[rpc\]" $HOME/.0gchain/config/config.toml | egrep -o ":[0-9]+") && \
+            PORT=${PORT#:} && \
+            sudo ufw allow $PORT/tcp &>/dev/null
+            sudo systemctl restart 0gchaind &>/dev/null
+            sleep 5
+
+            # Son kez RPC bağlantısını kontrol et
+            check_rpc_connection
+        fi
+    fi
+done
+
+# RPC bağlantısı başarılı oldu
+echo -e "\n\033[1;32mRPC BAĞLANTISI BAŞARILI\033[0m"
+echo -e "Public RPC URL'si: $RPC_URL"
+echo -e "\nBeni X'te takip edin: https://x.com/brsbtc"
