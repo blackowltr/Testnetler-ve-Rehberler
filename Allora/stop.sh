d86e4041f4bd5a4fd90147cf643bef114727a5fe
BlackOwl <107190154+blackowltr@users.noreply.github.com>
2024-07-04 02:48:25 +0300

    Update stop.sh

--- Allora/stop.sh
+++ Allora/stop.sh
@@ -1,13 +1,18 @@
 #!/bin/bash
 
-# Stop and remove specific containers
-docker stop $(docker ps -q --filter "name=worker-basic-eth-pred" --filter "name=updater-basic-eth-pred" --filter "name=inference-basic-eth-pred")
-docker rm $(docker ps -a -q --filter "name=worker-basic-eth-pred" --filter "name=updater-basic-eth-pred" --filter "name=inference-basic-eth-pred")
+# Stop and remove specific containers if they exist
+if docker ps -q --filter "name=worker-basic-eth-pred" --filter "name=updater-basic-eth-pred" --filter "name=inference-basic-eth-pred" 2>/dev/null; then
+    echo -e "\e[32mStopping and removing specified containers...\e[0m"
+    docker stop $(docker ps -q --filter "name=worker-basic-eth-pred" --filter "name=updater-basic-eth-pred" --filter "name=inference-basic-eth-pred")
+    docker rm $(docker ps -a -q --filter "name=worker-basic-eth-pred" --filter "name=updater-basic-eth-pred" --filter "name=inference-basic-eth-pred")
+else
+    echo -e "\e[32mİlgili container  bulunamadı. Tüm containerlar zaten durduruldu ve ardından kaldırıldı.\e[0m"
+fi
 
 # Get container IDs related to Allora
 container_ids=$(docker ps -a --filter "ancestor=alloranetwork/allora-inference-base-head" -q)
 
-# Stop and remove each Allora container
+# Stop and remove each Allora container if they exist
 for container_id in $container_ids; do
     echo "Stopping Allora container: $container_id"
     docker stop $container_id
