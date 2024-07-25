9485f02f058c9160e86bb1c7cc8af29ca69ab10a
BlackOwl <107190154+blackowltr@users.noreply.github.com>
2024-07-25 14:37:47 +0300

    Update sonaric_en.sh

--- Sonaric/sonaric_en.sh
+++ Sonaric/sonaric_en.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 
-# Colorful and highlighted opening message
+# Günlük dosyası oluşturma
+LOGFILE="/tmp/sonaric_install.log"
+exec > >(tee -i $LOGFILE)
+exec 2>&1
+
+# açılış
 echo -e ''
 echo -e '\e[40m\e[92m'
 echo ' ██████╗ ██╗      █████╗  ██████╗██╗  ██╗ ██████╗ ██╗    ██╗██╗'     
@@ -15,7 +20,7 @@ echo ''
 echo -e '\e[33mWelcome to the Sonaric Node Installation Guide!\e[0m'
 echo -e '\e[0m'
 
-# System Requirements
+# Sistem Gereksinimleri
 echo -e '\e[34mRequired System Hardware\e[0m'
 echo -e '\e[34m| Components   | Minimum Requirements |\e[0m'
 echo -e '\e[34m|--------------|-----------------------|\e[0m'
@@ -25,7 +30,7 @@ echo -e '\e[34m| Storage      | 20 GB SSD             |\e[0m'
 echo -e '\e[34m| Operating System | Ubuntu 22            |\e[0m'
 echo -e '\e[0m'
 
-# System update and required package installation
+# Sistem güncellemesi ve gerekli paketlerin kurulumu
 echo -e '\e[34mSystem Update and Package Installation\e[0m'
 sudo apt update && sudo apt upgrade -y
 if [ $? -ne 0 ]; then
@@ -41,7 +46,7 @@ if [ $? -ne 0 ]; then
 fi
 echo -e '\e[32mRequired packages installed successfully.\e[0m'
 
-# Download and run the Sonaric installation script
+# Sonaric kurulum scriptini indirme ve çalıştırma
 echo -e '\e[34mDownloading and Running Sonaric Installation Script\e[0m'
 wget https://raw.githubusercontent.com/Testnetnodes/Sonaric-Network/main/sonaric.sh && chmod +x sonaric.sh && ./sonaric.sh
 if [ $? -ne 0 ]; then
@@ -50,7 +55,7 @@ if [ $? -ne 0 ]; then
 fi
 echo -e '\e[32mSonaric installation script executed successfully.\e[0m'
 
-# Check if the node is successfully installed
+# Node'un başarıyla kurulduğunu kontrol etme
 echo -e '\e[34mChecking Node Installation\e[0m'
 sonaric node-info
 if [ $? -ne 0 ]; then
@@ -59,7 +64,7 @@ if [ $? -ne 0 ]; then
 fi
 echo -e '\e[32mNode installation checked successfully.\e[0m'
 
-# Run the GUI
+# GUI çalıştırma
 echo -e '\e[34mRunning the GUI\e[0m'
 IP=$(wget -qO- eth0.me)
 echo -e '\e[33mPlease enter your server password to proceed.\e[0m'
@@ -70,7 +75,7 @@ if [ $? -ne 0 ]; then
 fi
 echo -e '\e[32mGUI ran successfully.\e[0m'
 
-# Backup server information
+# Sunucu bilgilerini yedekleme
 echo -e '\e[34mBacking Up Server Information\e[0m'
 sonaric identity-export -o mysonaric.identity
 if [ $? -ne 0 ]; then
@@ -79,7 +84,7 @@ if [ $? -ne 0 ]; then
 fi
 echo -e '\e[32mServer information backed up successfully.\e[0m'
 
-# Ask the user if they want to change the Moniker name
+# Kullanıcıya Moniker ismini değiştirmek isteyip istemediğini sorma
 echo -e '\e[34mDo you want to change the Moniker name? (y/n)\e[0m'
 read -p "Your answer: " change_moniker
 
@@ -92,5 +97,5 @@ if [ "$change_moniker" == "y" ]; then
     echo -e '\e[32mMoniker name changed successfully.\e[0m'
 fi
 
-# Closing message
-echo -e '\e[32mIf you encounter any issues, please contact me for support and don't forget to follow me on X: https://x.com/brsbtc\e[0m'
+# Kapanış mesajı
+echo -e '\e[32mIf you encounter any issues, please contact me for support and don\'t forget to follow me on X: https://x.com/brsbtc\e[0m'
