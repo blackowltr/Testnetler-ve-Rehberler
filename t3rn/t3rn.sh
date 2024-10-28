88aebe514b04d3b18ef36d6c2cb360ded3400f51
BlackOwl <107190154+blackowltr@users.noreply.github.com>
2024-10-28 22:57:41 +0300

    Update t3rn.sh

--- t3rn/t3rn.sh
+++ t3rn/t3rn.sh
@@ -11,7 +11,7 @@ sudo apt install curl git wget htop tmux build-essential jq make lz4 gcc unzip -
 # Executor binary'sini indirin ve çıkarın
 wget https://github.com/t3rn/executor-release/releases/download/v0.21.10/executor-linux-v0.21.10.tar.gz
 tar -xvzf executor-linux-v0.21.10.tar.gz
-cd /root/executor/executor/bin
+cd executor-linux-v0.21.10/bin  # Dizin yolunu güncelleyin
 
 # Testnet ayarları
 export NODE_ENV=testnet
@@ -27,16 +27,13 @@ export EXECUTOR_PROCESS_CLAIMS=true
 # Kullanıcının girdiği private key'i ayarlayın
 export PRIVATE_KEY_LOCAL=$PRIVATE_KEY_LOCAL
 
-# Arbitrum Sepolia için RPC endpointleri
+# Ağları etkinleştirin
+export ENABLED_NETWORKS='arbitrum-sepolia,base-sepolia,blast-sepolia,optimism-sepolia'
+
+# RPC endpointlerini ayarlayın
 export RPC_ENDPOINTS_ARBT='https://api.zan.top/arb-sepolia'
-
-# Base Sepolia için RPC endpointleri
 export RPC_ENDPOINTS_BSSP='https://base-sepolia.blockpi.network/v1/rpc/public'
-
-# Blast Sepolia için RPC endpointleri
 export RPC_ENDPOINTS_BLSS='https://endpoints.omniatech.io/v1/blast/sepolia/public'
-
-# Optimism Sepolia için RPC endpointleri
 export RPC_ENDPOINTS_OPSP='https://endpoints.omniatech.io/v1/op/sepolia/public'
 
 # RPC ile sipariş kabul etme
